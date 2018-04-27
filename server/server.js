09744454063a067554b86a60c29c0d78eb272053
jxu14 <jeremyxu123@gmail.com>
2018-04-26 22:12:00 -0700

    added file upload to the post route, but still a throw err on pc at ./services/update/modifiedPaths so cannot test'

--- server/server.js
+++ server/server.js
@@ -35,10 +35,7 @@ app.get('/', (req, res) => {
   // res.send("GET /");
 })
 
-app.post('/dogs', upload.single('picture'), (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-});
+
 
 //-------------------middle ware---------------------
 
@@ -68,9 +65,12 @@ app.get(`/dogs/home`,(req,res)=>{
 })
 
 
+// app.post('/dogs', upload.single('picture'), (req, res) => {
+//   console.log(req.body);
+//   console.log(req.file);
+// });
 
-
-app.post('/dogs', (req, res) => {
+app.post('/dogs', upload.single('picture'), (req, res) => {
   let name = req.body.name;
   let age = req.body.age;
   let picture = req.body.picture;
@@ -79,7 +79,7 @@ app.post('/dogs', (req, res) => {
   if(!req.body.name||!req.body.age){
     console.log("bad request");
     res.status(400).send();
-  }else{
+  } else {
     const dog = new Dog({
       name: req.body.name,
       age: req.body.age,
@@ -181,134 +181,3 @@ app.get(`/dogs/:id/update`,(req,res)=>{
 app.listen(port, () => {
   console.log( `Listening on port ${port}`);
 })
-//
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-// const {Dog} = require('./models/dog.js');
-// const hbs = require(`hbs`);
-// const path = require(`path`);
-// const _ = require(`lodash`);
-// const methodOverride = require(`method-override`);
-// const app = express();
-//
-// //hbs set up
-// app.set('view engine','hbs');
-// app.set('views',path.join(__dirname,'../app/views'));
-// hbs.registerPartials(path.join(path.join(__dirname,'../app/views/partials')));
-// //setup
-// const port = process.env.PORT || 3000;
-// mongoose.Promise = global.Promise;
-// const database = process.env.MONGODB_URI || "mongodb://localhost:27017/Doglist"
-// mongoose.connect(database)
-//   .then(() => {
-//     console.log('connected to database');
-//   }).catch(() => {
-//     console.log('unable to connect to database');
-//   })
-// //-------------------middle ware---------------------
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(methodOverride(`_method`));
-// app.use(express.static(path.join(__dirname,`../public`)))
-// app.get('/', (req, res) => {
-//   res.redirect(`/dogs/home`)
-//   // res.send("GET /");
-// })
-//
-// //-------------------middle ware---------------------
-//
-// app.get('/dogs',(req,res)=>{
-//
-//   Dog.find()
-//   .then(dogs=>{
-//     res.send(dogs);
-//   })
-//   .catch(e=>{
-//     res.status(404).send();
-//   })
-// })
-//
-// app.get(`/dogs/new`,(req,res)=>{
-//   res.render(`./dogs/new`);
-// });
-// app.get(`/dogs/home`,(req,res)=>{
-//   Dog.find()
-//   .then((dogs) => {
-//     res.render(`./dogs/home`,{dogs})
-//   })
-// })
-//
-//
-//
-//
-// app.post('/dogs', (req, res) => {
-//   let name = req.body.name;
-//   let age = req.body.age;
-//   let picture = req.body.picture;
-//   let personality = req.body.personality;
-//   let description = req.body.description;
-//   if(!req.body.name||!req.body.age){
-//     console.log("bad request");
-//     res.status(400).send();
-//   }else{
-//     const dog = new Dog({
-//       name: req.body.name,
-//       age: req.body.age,
-//       picture: req.body.picture,
-//       personality: req.body.personality,
-//       description: req.body.description
-//     })
-//     dog.save()
-//     .then(dog => {
-//       res.redirect('/dogs/home');
-//     })
-//     .catch(e => {
-//       res.status(400).send();
-//     })}
-//   })
-//
-//
-// app.delete('/dogs/:id',(req,res)=>{
-//   const id = req.params.id;
-//   Dog.findByIdAndRemove(id)
-//     .then(Dogs => {
-//       res.redirect('home')
-//     }).catch (e => {
-//       res.status(404).send(e);
-//     })
-//   })
-//
-//   app.patch('dogs/:id', (req, res) => {
-//     const id = req.params.id;
-//     const body = _.pick(req.body, ['name', 'age','description','personality','picture']);
-//
-//     Dog.findByIdAndUpdate(id, {$set: body}, {new: true})
-//       .then(Dog => {
-//         if (!Dog) {
-//           res.status(404).send()
-//         } else {
-//           res.send(Dog);
-//         }
-//
-//       }).catch(e => {
-//         res.status(404).send(e);
-//       })
-//   })
-//
-//
-//
-// app.get('/dogs/:id',(req,res)=>{
-//     const id= req.params.id;
-//     console.log(id);
-//     Dog.findById(id)
-//     .then(dog=>{
-//       console.log(dog);
-//     res.render('./dogs/show.hbs',{dog});
-//   }).catch(e=>{
-//       res.status(404).send(e);
-//     })
-// })
-// app.listen(port, () => {
-//   console.log( `Listening on port ${port}`);
-// })
